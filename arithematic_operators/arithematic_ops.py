ff73ee3936f7a5ed28adfb12adec83ac7d47b5f2
Tinoet <88379020+Tinoet@users.noreply.github.com>
2022-03-20 22:24:42 +0100

    Add files via upload

---
+++ arithematic_operators/arithematic_ops.py
@@ -0,0 +1,6 @@
+# float point nmber has decimals
+# operators include the following +_/,//% etc
+print(10*3)
+# use the  double slash(//) to get an integer, % returns the remainder of division, exponet ** used to return raise to power.
+# augmented assignment operator use the operator and the equal sign
+
